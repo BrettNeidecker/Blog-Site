ce507b8cbba29ce5e41519cb96ab381264f15d98
BrettNeidecker <bneidecker@gmail.com>
2024-11-08 03:31:07 -0500

    Enabled CORS on the back-end

--- blog-backend/server.js
+++ blog-backend/server.js
@@ -3,9 +3,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRoutes = require('./routes/auth');
 const postRoutes = require('./routes/posts');
+const cors = require('cors'); //Import cors
 
 const app = express();
 app.use(express.json()); // Middleware to parse JSON
+
+//Allow requests from specific origins
+app.use(cors({
+    origin: ['http://localhost:3000', 'https://dulcet-strudel-229609.netlify.app'], // Frontend URLs
+    methods: 'GET,POST,PUT,DELETE', // Specify allowed methods
+    credentials: true,              // Enable if you need to include cookies
+}));
 
 // Connect to MongoDB
 mongoose.connect(process.env.MONGO_URI, {
